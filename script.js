6411617b2dc7492848f37d04081ae8516ab2854d
Alex <alexanderalex943@gmail.com>
2024-04-21 22:11:15 +0100

    add other game logic
    this includes player and computer counter also displays the winner that reaches 5 points first

--- script.js
+++ script.js
@@ -1,39 +1,55 @@
+const rock = document.querySelector("#btn-rock");
+const paper = document.querySelector("#btn-paper");
+const scissors = document.querySelector("#btn-scissors");
+const showResult = document.querySelector(".result");
+const playerScoreDisplay = document.querySelector("#player-score");
+const computerScoreDisplay = document.querySelector("#computer-score");
+
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-    let arr = ["rock", "paper", "scissors"]
-    return arr[Math.floor(Math.random() * 3)]
+  let arr = ["rock", "paper", "scissors"];
+  return arr[Math.floor(Math.random() * 3)];
 }
 
+function playRound(playerSelection) {
+  const computerChoice = getComputerChoice();
 
+  if (playerSelection === computerChoice) {
+    showResult.textContent = "It's a tie";
+  } else if (
+    (playerSelection === "rock" && computerChoice === "scissors") ||
+    (playerSelection === "paper" && computerChoice === "rock") ||
+    (playerSelection === "scissors" && computerChoice === "paper")
+  ) {
+    showResult.textContent = "You win!!";
+    playerScore++;
+  } else {
+    showResult.textContent = "You lose!!";
+    computerScore++;
+  }
 
-function getPlayerSelection() {
-    return prompt("choose rock, paper or scissors").toLocaleLowerCase()
+  updateScoreDisplay();
+
+  if (playerScore === 5 || computerScore === 5) {
+    announceWinner();
+  }
 }
 
-
-function playRound(computerChoice, playerSelection) {
-    if (playerSelection === computerChoice) {
-        return "it's a tie"
-    } else if (playerSelection === "rock" && computerChoice === "scissors" ||
-        playerSelection === "paper" && computerChoice === "rock" ||
-        playerSelection === "scissors" && computerChoice === "paper") {
-        return "You win!!"
-    }else {
-        return "You loose!!"
-    }
+function updateScoreDisplay() {
+  playerScoreDisplay.textContent = `Player: ${playerScore}`;
+  computerScoreDisplay.textContent = `Computer: ${computerScore}`;
 }
 
-
-
-
-
- 
-
-function playGame(){
-    for (let i=0; i <5; i++){
-        console.log(playRound(getComputerChoice(), getPlayerSelection()))
-    }
+function announceWinner() {
+  if (playerScore === 5) {
+    showResult.textContent = "You win the game!";
+  } else {
+    showResult.textContent = "Computer wins the game!";
+  }
 }
 
-
-playGame()
-
+rock.addEventListener("click", () => playRound("rock"));
+paper.addEventListener("click", () => playRound("paper"));
+scissors.addEventListener("click", () => playRound("scissors"));
